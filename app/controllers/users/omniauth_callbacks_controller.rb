abc0b974f16a1d12f5aa6d5f6a0353632e214b6e
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-02-17 23:51:32 +0900

    ユーザ編集をpasswardなしで登録できるように

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   # common callback method
   def callback_for(provider)
-    p request.env["omniauth.auth"]
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
