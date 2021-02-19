d084b7c8b54116b1582e9c951d629d9d5b821cb1
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-02-20 05:24:59 +0900

    section newページ作成

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,5 +1,32 @@
 class SectionsController < ApplicationController
+  before_action :sign_in_required
+
+  def new
+    @section = Section.new
+  end
+
   def index
     @sections = Section.all
   end
+
+  def create
+    @section = Section.new(section_params)
+    if @section.save
+      flash[:notice] = "OKRを登録しました"
+      redirect_to sections_path
+    else
+      flash[:alert] = ErrorFormatter.format(@section)
+      render 'new'
+    end
+  end
+
+  private
+
+    # def set_section
+    #   @section = SectionOkr.find(params[:id])
+    # end
+
+    def section_params
+      params.require(:section).permit(:name)
+    end
 end
