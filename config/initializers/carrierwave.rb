dcad61c81663fdd40dbe2b6b754e40f64ef9bfd7
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-02-20 17:39:45 +0900

    gemをfog_awsに変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,19 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: 'AKIAX4FI7CLFKBYVACPX',
       aws_secret_access_key: '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',
-      region: 'ap-northeast-1'
+      region: 'ap-northeast-1',
+      path_style: true
     }
 
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
     config.fog_directory  = 'okr-intern-s3'
-    config.cache_storage = :fog
   end
 end
