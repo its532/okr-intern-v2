93463568976fe21c5d6ad18d87863c0ca0845361
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-02-20 16:54:40 +0900

    omniouth失敗時にlogで出力できるように

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -39,6 +39,7 @@ Devise.setup do |config|
   require 'devise/orm/active_record'
 
   config.omniauth :google_oauth2,ENV['GOOGLE_APP_ID'],ENV['GOOGLE_APP_SECRET'],scope: 'email'
+  OmniAuth.config.logger = Rails.logger
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
