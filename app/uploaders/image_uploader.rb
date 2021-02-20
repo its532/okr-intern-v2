3ccf7ec4e374a6bf0436f880ab54a6c8ca009b37
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-02-20 20:25:40 +0900

    carrierwave経由の画像をs3に保存

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,10 +11,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     storage :fog
   end
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,12 +8,13 @@ unless Rails.env.development? || Rails.env.test?
       provider: 'AWS',
       aws_access_key_id: 'AKIAX4FI7CLFKBYVACPX',
       aws_secret_access_key: '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',
-      region: 'ap-northeast-1',
-      path_style: true
+      region: 'ap-northeast-1'
     }
 
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'okr-intern-s3'
+    config.fog_public = false
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/okr-intern-s3'
   end
 end
