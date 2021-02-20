20a53cb17fe7d3b5036eb017a5c60298e8e6ad18
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-02-21 06:04:36 +0900

    personal_okr indexページ作成

--- app/controllers/personal_okrs_controller.rb
+++ app/controllers/personal_okrs_controller.rb
@@ -1,2 +1,24 @@
 class PersonalOkrsController < ApplicationController
+  before_action :sign_in_required
+  # before_action :set_section_okr, only: %i[edit update destroy]
+
+  def new
+    @personal_okr = PersonalOkr.new
+    # @personal_okr.personal_key_results.build
+  end
+
+  def index
+    @personal_okrs = PersonalOkr.all
+  end
+
+
+  private
+
+    # def set_personal_okr
+    #   @personal_okr = PersonalOkr.find(params[:id])
+    # end
+
+    def personal_okr_params
+      params.require(:personal_okr).permit(:section_id, :objective, :objective_reason, :user_id, :quarter)
+    end
 end
