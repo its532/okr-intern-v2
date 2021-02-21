bbe6a2ecc26cc54e76831ffe0173479e9659177c
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-02-22 01:31:06 +0900

    モデルにpolymorphic追加

--- app/models/personal_okr.rb
+++ app/models/personal_okr.rb
@@ -1,3 +1,4 @@
 class PersonalOkr < ApplicationRecord
+  has_many :key_results, as: :okr
   belongs_to :section
 end
--- app/models/section_okr.rb
+++ app/models/section_okr.rb
@@ -1,3 +1,4 @@
 class SectionOkr < ApplicationRecord
+  has_many :key_results, as: :okr
   belongs_to :section
 end
