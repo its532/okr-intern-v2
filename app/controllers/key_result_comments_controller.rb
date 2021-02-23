de767c47009a89bf8f6e1fc4f83bd7758ed11ab9
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-02-24 04:08:45 +0900

    コメント新規追加typeによってpathを変えた

--- app/controllers/key_result_comments_controller.rb
+++ app/controllers/key_result_comments_controller.rb
@@ -1,6 +1,6 @@
 class KeyResultCommentsController < ApplicationController
   before_action :set_key_result, only: %i[new edit]
-  before_action :set_section_okr, only: %i[new edit]
+  before_action :set_okr, only: %i[new edit]
   # before_action :set_section_key_result_comment, only: %i[edit update destroy]
 
   def new
@@ -18,15 +18,32 @@ class KeyResultCommentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @key_result_comment.update(key_result_comment_params)
+      flash[:notice] = "コメントを更新しました"
+      # redirect_to section_key_results_path(section_okr_id: @section_key_result_comment.section_key_result.section_okr_id)
+    else
+      flash[:alert] = ErrorFormatter.format(@key_result_comment)
+      # redirect_to edit_section_key_result_comment_path(section_key_result_id: @section_key_result_comment.section_key_result.id)
+    end
+  end
+
   private
 
     def key_result_comment_params
       params.require(:key_result_comment).permit(:comment, :month, :measure, :key_result_id)
     end
 
-    def set_section_okr
-      @section_okr = SectionOkr.find_by(id: params[:okr_id])
-      p @section_okr
+    def set_okr
+      if params[:okr_type] == 'SectionOkr'
+        @okr = SectionOkr.find_by(id: params[:okr_id])
+      else
+        @okr = PersonalOkr.find_by(id: params[:okr_id])
+      end
+      p @okr
     end
 
     def set_key_result
