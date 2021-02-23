971ac142d136da74a4cf8f3f56f99872afb9316f
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-02-24 02:19:01 +0900

    section_okr コメント一覧ボタン設置

--- app/controllers/key_results_controller.rb
+++ app/controllers/key_results_controller.rb
@@ -1,5 +1,5 @@
 class KeyResultsController < ApplicationController
   def index
-    @key_results = KeyResult.where(okr_id: params[:okr_id])
+    @key_results = KeyResult.where(okr_type: params[:okr_type]).where(okr_id: params[:okr_id])
   end
 end
