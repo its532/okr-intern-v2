ec058db17af33fa0f56d54a86dfa52822cb92251
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-02-24 17:58:49 +0900

    コメントqごとの月を表示させるメソッド

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def select_months(quarter)
+    if quarter == 'Q1'
+      [['1月', '1月'], ['2月', '2月'], ['3月', '3月']]
+    elsif quarter == 'Q2'
+      [['4月', '4月'], ['5月', '5月'], ['6月', '6月']]
+    elsif quarter == 'Q3'
+      [['7月', '7月'], ['8月', '8月'], ['9月', '9月']]
+    else
+      [['10月', '10月'], ['11月', '11月'], ['12月', '12月']]
+    end
+  end
 end
