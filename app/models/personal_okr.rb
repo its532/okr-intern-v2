371c469ad868573ab17bf5b7bff80bb977fde54c
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-02-24 21:40:36 +0900

    personal_okr 一覧画面viewテーブル作成

--- app/models/personal_okr.rb
+++ app/models/personal_okr.rb
@@ -1,4 +1,5 @@
 class PersonalOkr < ApplicationRecord
+  belongs_to :user
   has_many :key_results, as: :okr
   belongs_to :section
   accepts_nested_attributes_for :key_results, allow_destroy: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable, :trackable, :omniauthable, omniauth_providers: %i[google_oauth2]
 
   mount_uploader :image, ImageUploader
+  has_many :personal_okrs
 
   protected
 
