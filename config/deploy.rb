6030cfb82d3684f54ec5ec2e61a5ca555d3d55e8
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-02-26 03:50:54 +0900

    yarn option追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,9 @@ set :rbenv_ruby, '2.7.1'
 
 set :log_level, :debug
 
+set :yarn_flags, '--prefer-offline --production'
+set :yarn_roles, :app
+
 namespace :deploy do
   desc 'Restart application'
   task :restart do
@@ -30,6 +33,17 @@ namespace :deploy do
       with rails_env: fetch(:rails_env) do
         within current_path do
           execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+
+  desc 'Upgrade yarn'
+  task :yarn_upgrade do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :yarn, :upgrade
         end
       end
     end
