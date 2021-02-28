2831b0c74e57aa1ff3221fc4cd1bbef1195777eb
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-03-01 02:58:44 +0900

    toppage一覧ページ作成

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,5 +2,6 @@ class ToppagesController < ApplicationController
   before_action :sign_in_required, only: [:index]
 
   def index
+    @personal_okrs = current_user.personal_okrs.order(year: :desc).order(:quarter)
   end
 end
