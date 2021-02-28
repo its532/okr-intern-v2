15af4b44a165cf9f3819c31ccb28eca6e38cd4b1
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-02-28 15:57:25 +0900

    personal_okr 同一quarter登録できないように

--- app/models/personal_okr.rb
+++ app/models/personal_okr.rb
@@ -2,4 +2,6 @@ class PersonalOkr < ApplicationRecord
   belongs_to :user
   has_many :key_results, as: :okr
   accepts_nested_attributes_for :key_results, allow_destroy: true
+
+  validates :user_id,  uniqueness: { scope: [:quarter], message: "のOKRは既に登録されています"  }
 end
