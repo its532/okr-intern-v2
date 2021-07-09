ffb3fe203bb1b59185d486367f4bebdfac779c1c
ituski kikuyama <tf.k.itsuki.rits@gmail.com>
2021-07-09 17:23:52 +0900

    fix: 関連テーブルも含めて削除できるように

--- app/models/section_okr.rb
+++ app/models/section_okr.rb
@@ -1,5 +1,6 @@
 class SectionOkr < ApplicationRecord
   has_many :key_results, as: :okr
+  has_many :section_okr_votes, dependent: :destroy
   has_many :users, through: :section_okr_votes
   accepts_nested_attributes_for :key_results, allow_destroy: true
 
